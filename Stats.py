690044f2beabcef90554fc974d9aab4a90b70290
sabernstein <sadiebernstein2023@u.northwestern.edu>
2023-03-05 16:16:52 -0600

    finalized results

--- Stats.py
+++ Stats.py
@@ -51,8 +51,19 @@ dicts = [dp.extrovert_counts,
          dp.extroverts_who_got_extrovert_ad_untargeted_counts,
          dp.extroverts_who_got_introvert_ad_targeted_counts,
          dp.extroverts_who_got_extrovert_ad_targeted_counts,
-         #TODO add any other dictionaries we add here 
-         ]
+         dp.extroverts_who_got_targeted_counts,
+         dp.extroverts_who_got_untargeted_counts,
+         dp.introverts_who_got_targeted_counts,
+         dp.introverts_who_got_untargeted_counts,
+         dp.targeted_counts,
+         dp.untargeted_counts,
+         dp.matched_untargeted_counts,
+         dp.matched_targeted_counts,
+         dp.unmatched_untargeted_counts,
+         dp.unmatched_targeted_counts,
+         dp.matched_counts,
+         dp.unmatched_counts]
+
 t_test_results = {}
 
 for d1 in dicts:
@@ -63,12 +74,36 @@ for d1 in dicts:
         test_name = d1['Name'] + ' vs. ' + d2['Name']
         t_test_results[test_name] = {'f': f, 'p': p}
 
+# print(t_test_results)
+final_results = {}
+# Whether or not the ad is matched to self-perception is significant 
+# Whether or not the ad is targeted has less of a difference
 
+# Was there significance between extroverts who were matched vs. extroverts who were unmatched? 
+final_results["ExtrovertsWhoGotExtrovertAd vs. ExtrovertsWhoGotIntrovertAd"] = t_test_results["ExtrovertsWhoGotExtrovertAd vs. ExtrovertsWhoGotIntrovertAd"]
+# Was there significance between introverts who were matched vs. introverts who were unmatched? 
+final_results["IntrovertsWhoGotExtrovertAd vs. IntrovertsWhoGotIntrovertAd"] = t_test_results["IntrovertsWhoGotExtrovertAd vs. IntrovertsWhoGotIntrovertAd"]
+# Was there signficiance between general people who were matched vs. unmatched?
+final_results["UnMatched vs. Matched"] = t_test_results["UnMatched vs. Matched"]
+# Was there significance between extroverts who got targeted ads vs. extroverts who got untargeted ads?
+final_results["ExtrovertsWhoGotTargeted vs. ExtrovertsWhoGotUnTargeted"] = t_test_results["ExtrovertsWhoGotTargeted vs. ExtrovertsWhoGotUnTargeted"]
+# Was there significance between introverts who got targeted ads vs. introverts who got untargeted ads?
+final_results["IntrovertsWhoGotTargeted vs. IntrovertsWhoGotUnTargeted"] = t_test_results["IntrovertsWhoGotTargeted vs. IntrovertsWhoGotUnTargeted"]
+# Was there significance between general people who were targeted vs. untargeted?
+final_results["Targeted vs. UnTargeted"] = t_test_results["Targeted vs. UnTargeted"]
+# Was there signficance betweeen matched and targeted vs. matched and untargeted?
+final_results["MatchedTargeted vs. MatchedUnTargeted"] = t_test_results["MatchedTargeted vs. MatchedUnTargeted"]
+# Was there signficance betweeen unmatched and targeted vs. unmatched and untargeted?
+final_results["UnMatchedTargeted vs. UnMatchedUnTargeted"] = t_test_results["UnMatchedTargeted vs. UnMatchedUnTargeted"]
+# Was there signficance betweeen matched and untargeted vs. unmatched and untargeted?
+final_results["MatchedUnTargeted vs. UnMatchedUnTargeted"] = t_test_results["MatchedUnTargeted vs. UnMatchedUnTargeted"]
+
+print(final_results)
 significant_test_reults = {}
-for i in t_test_results.keys():
-    if t_test_results[i]['p'] <= 0.05:
-        significant_test_reults[i] = t_test_results[i]
-        # print(i, '     ', t_test_results[i])
+for i in final_results.keys():
+    if final_results[i]['p'] <= 0.05:
+        significant_test_reults[i] = final_results[i]
+        print(i, '     ', final_results[i])
 
 
 
